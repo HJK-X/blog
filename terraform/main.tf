930356ae5392b9af4fff20db78e49df6a114978d
HJK-X <34551787+HJK-X@users.noreply.github.com>
2025-12-21 23:20:35 -0800

    update records for domain

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,14 @@
 locals {
-  domain_name  = "justinkim.app"
+  domain       = "justinkim.app"
   github_owner = "HJK-X"
   github_repo  = "blog"
 }
 
+data "cloudflare_zones" "main" {
+  filter {
+    name = local.domain
+  }
+}
 
 module "landing_page" {
   source = "./modules/cloudflare-pages"
@@ -16,4 +21,19 @@ module "landing_page" {
 
   root_dir        = "landing"
   destination_dir = "public"
+}
+
+resource "cloudflare_pages_domain" "landing_domain" {
+  account_id   = var.cloudflare_account_id
+  project_name = module.landing_page.name
+  domain       = local.domain
+}
+
+resource "cloudflare_record" "landing_cname" {
+  zone_id = data.cloudflare_zones.main.zones[0].id
+  name    = "@"
+  type    = "CNAME"
+  content = "${module.landing_page.name}.pages.dev"
+  ttl     = 1
+  proxied = true
 }
--- terraform/modules/cloudflare-pages/output.tf
+++ terraform/modules/cloudflare-pages/output.tf
@@ -1,3 +1,6 @@
+output "name" {
+  value = var.name
+}
 output "pages_subdomain" {
   value = cloudflare_pages_project.this.subdomain
 }
